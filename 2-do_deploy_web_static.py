12e123968003b0942de5b46d7fd6142af296ea1f
MairaQuillQuest <ririsoniah@gmail.com>
2024-04-04 17:47:23 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-'''
-Deploy files to remote server using Fabric
+"""Do deploy web static module"""
+import os
+from fabric import api
 
-'''
-from fabric.api import env, put, run
-import os.path
-env.hosts = ['34.229.255.107', '18.204.7.214']
-
+api.env.user = 'ubuntu'
+api.env.hosts = ['34.229.255.107', '18.204.7.214']
 
 def do_deploy(archive_path):
-    '''Upload achive to web servers'''
-    if not os.path.isfile(archive_path):
+    """
+    Fabric script that distributes an archive to web servers using do_deploy
+    """
+    if not os.path.exists(archive_path):
         return False
     try:
-        filename = archive_path.split('/')[-1]
-        no_ext = filename.split('.')[0]
-        path_no_ext = '/data/web_static/releases/{}/'.format(no_ext)
-        symlink = '/data/web_static/current'
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(path_no_ext))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, path_no_ext))
-        run('rm /tmp/{}'.format(filename))
-        run('mv {}web_static/* {}'.format(path_no_ext, path_no_ext))
-        run('rm -rf {}web_static'.format(path_no_ext))
-        run('rm -rf {}'.format(symlink))
-        run('ln -s {} {}'.format(path_no_ext, symlink))
+        file_name = os.path.basename(archive_path)
+        just_name = os.path.splitext(file_name)[0]
+        api.put(archive_path, "/tmp/")
+        api.run("mkdir -p /data/web_static/releases/{}".format(just_name))
+        api.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
+                                                        file_name, just_name))
+        api.run("rm /tmp/{}".format(file_name))
+        path_r = "/data/web_static/releases/"
+        api.run('mv {0}{1}/web_static/* {0}{1}/'.format(path_r, just_name))
+        api.run('rm -rf /data/web_static/releases/{}/web_static'.format(
+                                                                just_name))
+        api.run("rm -rf /data/web_static/current")
+        api.run("ln -s /data/web_static/releases/{} \
+                    /data/web_static/current".format(just_name))
         return True
-    except Exception:
+    except Exception as e:
+        print("Exception:", e)
         return False
