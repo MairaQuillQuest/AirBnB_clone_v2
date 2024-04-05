e674a3348d4371670214270920c0993872773cbe
MairaQuillQuest <ririsoniah@gmail.com>
2024-04-04 17:56:43 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,49 @@
 #!/usr/bin/python3
-"""Do deploy web static module"""
-import os
-from fabric import api
+from fabric.api import *
+from os.path import exists
+from datetime import datetime
+from fabric.api import local
 
-api.env.user = 'ubuntu'
-api.env.hosts = ['34.229.255.107', '18.204.7.214']
+env.hosts = ['34.229.255.107', '18.204.7.214']
+
+
+def do_pack():
+    '''
+    Fabric script that generates a .tgz archive from the
+    contents of the web_static
+    '''
+    try:
+        filepath = 'versions/web_static_' + datetime.now().\
+                   strftime('%Y%m%d%H%M%S') + '.tgz'
+        local('mkdir -p versions')
+        local('tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+        web_static')
+        print('web_static packed: {} -> {}'.
+              format(filepath, os.path.getsize(filepath)))
+    except:
+        return None
+
 
 def do_deploy(archive_path):
+        """
+        Depploy to yoru webs server
     """
-    Fabric script that distributes an archive to web servers using do_deploy
-    """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        file_name = os.path.basename(archive_path)
-        just_name = os.path.splitext(file_name)[0]
-        api.put(archive_path, "/tmp/")
-        api.run("mkdir -p /data/web_static/releases/{}".format(just_name))
-        api.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-                                                        file_name, just_name))
-        api.run("rm /tmp/{}".format(file_name))
-        path_r = "/data/web_static/releases/"
-        api.run('mv {0}{1}/web_static/* {0}{1}/'.format(path_r, just_name))
-        api.run('rm -rf /data/web_static/releases/{}/web_static'.format(
-                                                                just_name))
-        api.run("rm -rf /data/web_static/current")
-        api.run("ln -s /data/web_static/releases/{} \
-                    /data/web_static/current".format(just_name))
-        return True
-    except Exception as e:
-        print("Exception:", e)
-        return False
+        if exists(archive_path) is False:
+            return False
+        file_name = archive_path.split('/')[1]
+        file_path = '/data/web_static/releases'
+        try:
+            put(archive_path, '/tmp/')
+            run('mkdir -p {}{}'.format(file_path, file_name[:-4]))
+            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                                   file_path, file_name[:-4]))
+            run('rm /tmp/{}'.format(file_name))
+            run('mv {}{}/web_static/* {}{}/'.format(file_path, file_name[:-4],
+                                                    file_path, file_name[:-4]))
+            run('rm -rf {}{}/web_static'.format(file_path, file_name[:-4]))
+            run('rm -rf /data/web_static/current')
+            run('ln -s {}{}/ /data/web_static/current'.format(file_path,
+                                                              file_name[:-4]))
+            return True
+        except:
+            return False
