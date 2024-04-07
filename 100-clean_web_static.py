68d05e1972a43b8aba6e378914d6f5120d895ac8
MairaQuillQuest <ririsoniah@gmail.com>
2024-04-07 01:01:39 -0700

    [Feature]Adding AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,62 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
-import os
-from fabric.api import *
+'''
+Deploy files to remote server using Fabric
 
-env.hosts = ["34.229.255.107", "18.204.7.214"]
+'''
+from fabric.api import env, put, run, local
+import os.path
+from time import strftime
+env.hosts = ['34.229.255.107', '18.204.7.214']
+
+
+def do_pack():
+    '''Generate required files'''
+    timenow = strftime('%Y%M%d%H%M%S')
+    try:
+        local('mkdir -p versions')
+        filename = 'versions/web_static_{}.tgz'.format(timenow)
+        local('tar -czvf {} web_static/'.format(filename))
+        return filename
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    '''Upload achive to web servers'''
+    if not os.path.isfile(archive_path):
+        return False
+    try:
+        filename = archive_path.split('/')[-1]
+        no_ext = filename.split('.')[0]
+        path_no_ext = '/data/web_static/releases/{}/'.format(no_ext)
+        symlink = '/data/web_static/current'
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(path_no_ext))
+        run('tar -xzf /tmp/{} -C {}'.format(filename, path_no_ext))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {}web_static/* {}'.format(path_no_ext, path_no_ext))
+        run('rm -rf {}web_static'.format(path_no_ext))
+        run('rm -rf {}'.format(symlink))
+        run('ln -s {} {}'.format(path_no_ext, symlink))
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    '''Deploy to the web server'''
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    deployment = do_deploy(archive_path)
+    return deployment
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    '''Clean webservers'''
+    __local = 'versions/*.tgz'
+    __run = '/data/web_static/releases/web_static*'
+    if number == 0:
+        number = 1
+    local("rm -f `ls -t {} | awk 'NR>{}'`".format(__local, number))
+    run("rm -rf `ls -td {} | awk 'NR>{}'`".format(__run, number))
