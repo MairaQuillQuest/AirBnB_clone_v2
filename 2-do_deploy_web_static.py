602476df315469bfc3f051bf2c45430522ee4159
MairaQuillQuest <ririsoniah@gmail.com>
2024-04-07 11:45:59 -0700

    [Feature]Adding AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
-from fabric.api import *
-from os.path import exists
-from datetime import datetime
-from fabric.api import local
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-env.hosts = ['34.229.255.107', '18.204.7.214']
-
-
-def do_pack():
-    '''
-    Fabric script that generates a .tgz archive from the
-    contents of the web_static
-    '''
-    try:
-        filepath = 'versions/web_static_' + datetime.now().\
-                   strftime('%Y%m%d%H%M%S') + '.tgz'
-        local('mkdir -p versions')
-        local('tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
-        web_static')
-        print('web_static packed: {} -> {}'.
-              format(filepath, os.path.getsize(filepath)))
-    except:
-        return None
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_deploy(archive_path):
-        """
-        Depploy to yoru webs server
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-        if exists(archive_path) is False:
-            return False
-        file_name = archive_path.split('/')[1]
-        file_path = '/data/web_static/releases'
-        try:
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}{}'.format(file_path, file_name[:-4]))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
-                                                   file_path, file_name[:-4]))
-            run('rm /tmp/{}'.format(file_name))
-            run('mv {}{}/web_static/* {}{}/'.format(file_path, file_name[:-4],
-                                                    file_path, file_name[:-4]))
-            run('rm -rf {}{}/web_static'.format(file_path, file_name[:-4]))
-            run('rm -rf /data/web_static/current')
-            run('ln -s {}{}/ /data/web_static/current'.format(file_path,
-                                                              file_name[:-4]))
-            return True
-        except:
-            return False
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
