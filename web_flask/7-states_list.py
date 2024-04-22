8a49086fb3e33eb50611defca008e5e6b587d4bf
MairaQuillQuest <ririsoniah@gmail.com>
2024-04-21 19:46:07 -0700

    [Feature]: Adding AirBnB clone - Web framework Project

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/states_list')
-def state_list():
-    """
-    display a HTML page
-    H1: "States"
-    UL: list of all State objects
-       LI: description of one State:
-           <state.id>: <B> <state.name> </B>
-    """
-    state_objs = storage.all("State")
-    return render_template("7-states_list.html", states=state_objs)
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    '''
+    Lists states from db
+    '''
+    state_dict = storage.all('State')
+    state_list = []
+
+    for state in state_dict.values():
+        state_list.append(state)
+    return render_template('7-states_list.html', state_list=state_list)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """
-    Tears down the db connection
-    """
+def teardown_app(e):
+    '''
+    teardown app context
+    '''
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
